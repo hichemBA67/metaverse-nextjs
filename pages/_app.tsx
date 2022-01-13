2117fc011a89924c6fc12211379cfa276f0e2cb9
Hichem Ben Aoun <hichem.ben-aoun@slidemotif.com>
2022-01-13 10:00:37 +0100

    Login (Moralis)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+const appId = process.env.NEXT_PUBLIC_APP_ID;
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // @ts-ignore
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
